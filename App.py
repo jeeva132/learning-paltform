8b48028ef46a487f0ad8f0382d8c61c6f9e28c98
JEEVAN S <chuppijeevan@gmail.com>
2023-12-30 17:37:47 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash,session
 from flask_sqlalchemy import SQLAlchemy
-import pyotp
 from datetime import datetime
 
 app = Flask(__name__)
